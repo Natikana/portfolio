4fe0d8b039b7323f9b0a70a197656df8c153f8e1
Natikana <mashkovski82@gmail.com>
2023-04-16 14:26:29 +0200

    add third part adaptive portfolio

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -11,37 +11,40 @@ export const Contact = () => {
             <div className={containerStyle.mainContainer}>
                 <span className={titleStyle.title}>Contact</span>
                 <h2 className={titleStyle.mainTitle}>Get in Touch</h2>
-                <div className={cl.mapSection}>
-                    <div className={cl.map}>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d312779.9431310756!2d20.781016711291045!3d52.232606289062204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc669a869f01%3A0x72f0be2a88ead3fc!2sWarsaw!5e0!3m2!1sen!2spl!4v1681480882675!5m2!1sen!2spl"
-                            width="100%" height="355" style={{border:0}} loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <div className={cl.contactBlock}>
+                    <div className={cl.mapSection}>
+                        <div className={cl.map}>
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d312779.9431310756!2d20.781016711291045!3d52.232606289062204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc669a869f01%3A0x72f0be2a88ead3fc!2sWarsaw!5e0!3m2!1sen!2spl!4v1681480882675!5m2!1sen!2spl"
+                                width="100%" height="355" style={{border:0}} loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                    <div className={cl.fields}>
+                        <form >
+                            <div className={cl.form}>
+                                <div className={cl.first}>
+                                    <ul className={cl.input}>
+                                        <li className={cl.contactFields}>
+                                            <input id="name" type="text" placeholder="Name"/>
+                                        </li>
+                                        <li className={cl.contactFields}>
+                                            <input id="email" type="text" placeholder="Email"/>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={cl.last}>
+                                    <textarea id="message" placeholder="Message"></textarea>
+                                </div>
+                                <button className={btn.commonBtn}>
+                                    <span className={`${text.generelText} ${cl.textBtn}`}>Send message</span>
+                                </button>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
-                <div className={cl.fields}>
-                    <form >
-                        <div className={cl.form}>
-                            <div className={cl.first}>
-                                <ul className={cl.input}>
-                                    <li className={cl.contactFields}>
-                                        <input id="name" type="text" placeholder="Name"/>
-                                    </li>
-                                    <li className={cl.contactFields}>
-                                        <input id="email" type="text" placeholder="Email"/>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className={cl.last}>
-                                <textarea id="message" placeholder="Message"></textarea>
-                            </div>
-                            <button className={btn.commonBtn}>
-                                <span className={`${text.generelText} ${cl.textBtn}`}>Send message</span>
-                            </button>
-                        </div>
 
-                    </form>
-                </div>
             </div>
         </div>
     )
