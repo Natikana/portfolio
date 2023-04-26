91ca518411a3c5d0e9caff9391cc97afac794076
Natikana <mashkovski82@gmail.com>
2023-04-27 00:11:09 +0200

    update contact form portfolio

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,28 +1,44 @@
-import React, {DetailedHTMLProps, FormEvent, FormEventHandler, FormHTMLAttributes} from "react";
+import React, {ChangeEvent, DetailedHTMLProps, FormEvent, FormEventHandler, FormHTMLAttributes, useState} from "react";
 import cl from "./Contact.module.scss"
 import btn from "../common/styles/Common.module.scss";
 import text from "../common/styles/Common.module.scss";
 import sectionCommon from "../common/styles/Common.module.scss";
 import {Title} from "../common/components/title/Title";
 import Fade from "react-reveal/Fade";
-import { useRef } from 'react';
+import {useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 
 export const Contact = () => {
-    const form = useRef(null);
+    const [clean, setClean] = useState<string>('')
+    const [clean1, setClean1] = useState<string>('')
+    const [clean2, setClean2] = useState<string>('')
+    const [disabled, setDisabled] = useState<boolean>(false)
+    const [message, setMessage] = useState<string>('')
+    const [load, setLoad] = useState<'success' | 'failed' | null>(null)
 
-    const sendEmail = (e:FormEvent<HTMLFormElement>) => {
+    const form = useRef(null);
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        setDisabled(true)
         emailjs.sendForm('service_5s2l2af', 'template_czia9qw', form.current, 'nHa0h8657FwRch5W8')
-            .then((result:any) => {
-                console.log(result.text);
-                console.log('sent')
-            }, (error:any) => {
-                console.log(error.text);
-            });
+            .then((result: any) => {
+                    console.log(result.text);
+                    console.log('sent')
+                    setDisabled(false)
+                    setLoad('success')
+                    setTimeout(() => {
+                        setLoad(null)
+                    },4000)
+
+                },
+                (error: any) => {
+                    console.log(error.text);
+                    setLoad('failed')
+                });
     };
+
 
     return (<div id={'contact'} className={`${sectionCommon.section} ${cl.contact}`}>
             <Fade clear duration={3000}>
@@ -35,11 +51,28 @@ export const Contact = () => {
                             referrerPolicy="no-referrer-when-downgrade">
                         </iframe>
                     </div>
+
+                   {load === 'success' && <div className={cl.contactMessage}>Your message has been received, I will contact you soon.</div>}
+
                     <form className={cl.contactForm} ref={form} onSubmit={sendEmail}>
-                        <input className={cl.contactInput} id="name"  type="text" name="user_name" placeholder="Name"/>
-                        <input className={cl.contactInput}  id="email" type="text" name="user_email" placeholder="Email"/>
-                        <textarea className={cl.contactTextarea} id="message" name="message" placeholder="Message"></textarea>
-                        <button type={"submit"} value="Send" className={btn.commonBtn}>
+                        <input onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                            setClean(e.currentTarget.value)
+                        }} value={clean} className={cl.contactInput} id="name" type="text" name="user_name"
+                               placeholder="Name"/>
+                        <input onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                            setClean1(e.currentTarget.value)
+                        }} value={clean1} className={cl.contactInput} id="email" type="text" name="user_email"
+                               placeholder="Email"/>
+                        <textarea onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+                            setClean2(e.currentTarget.value)
+                        }} value={clean2} className={cl.contactTextarea} id="message" name="message"
+                                  placeholder="Message"></textarea>
+                        <button disabled={disabled} style={disabled ? {backgroundColor: 'darkgray'} : {backgroundColor: ''}}
+                                type={"submit"} value="Send" className={btn.commonBtn} onClick={() => {
+                            setClean('');
+                            setClean1('');
+                            setClean2('')
+                        }}>
                             <span className={`${text.generelText} ${cl.textBtn}`}>Send message</span>
                         </button>
                     </form>
